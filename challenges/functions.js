01dab918180e21f80928f20b28927165eeaa3bcd
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-06 11:10:24 -0700

    next thing

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,13 +6,24 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function compute (param1, param2, cd){
+  return cd(param1,param2)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2){
+  return num1 + num2;
+}
+function multiply(numb1,numb2){
+  return numb1 * numb2;
+}
+function greet(first_name, last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
